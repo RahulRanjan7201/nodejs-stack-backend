a8d044923ca41621c8ea05ed5f482a05b7b9f3de
Rahul Ranjan <rahul.ranjan7201@gmail.com>
2023-01-23 18:29:48 +0530

    Builds

--- __tests__/api-test/stack.spec.js
+++ __tests__/api-test/stack.spec.js
@@ -3,12 +3,12 @@ const app = require('../../app');
 describe('Put API', () => { 
     it('should respond with error message if value type is not number ' , async () => {
         const response = await request(app).put("/").send({value:"test John"});
-        expect(response.text).toBe('value should be of number type');
+        expect(response.body.message).toBe('value should be of number type');
         expect(response.status).toBe(400)
     })
     it('should respond with error message if value is not present ' , async () => {
         const response = await request(app).put("/").send();
-        expect(response.text).toBe('value should be present');
+        expect(response.body.message).toBe('value should be present');
         expect(response.status).toBe(400)
     })
  })
